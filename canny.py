c3aef8783ea70b232bd7765d2491b3631b086a52
root <root@DESKTOP-R3NKPLG.localdomain>
2025-08-22 17:02:42 -0600

    primera actualizacion

--- canny.py
+++ canny.py
@@ -1,11 +1,10 @@
+import cv2
 
-1  import cv2
-2
-3  img = cv2.imread('cartas.jpg',0)
-4  bordeCanny = cv2.Canny(img, 100, 200)
-5
-6  cv2.imshow('Original', img)
-7  cv2.imshow('blur', bordeCanny)
-8
-9  cv2.waitkey(0)
-10 cv2.destroyAllWindows()
+img = cv2.imread('cartas.jpeg',0)
+bordeCanny = cv2.Canny(img, 100, 200)
+
+cv2.imshow('Oringinal', img)
+cv2.imshow('blur', bordeCanny)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
